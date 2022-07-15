2f805c564ae0da779fb54fd823d58c4f0ad08c91
GabrielCarrijoF <carrijogit2003@gmail.com>
2022-07-14 23:46:06 -0300

    refactor: arrays

---
+++ src/main/java/Polimorfismo/br/com/byteBank/teste/TesteArrayReferencias.java
@@ -0,0 +1,19 @@
+package Polimorfismo.br.com.byteBank.teste;
+
+public class TesteArrayReferencias {
+
+    public static void main(String[] args) {
+
+        int[] idades = new int[5];
+
+        for (int i = 0; i < idades.length; i++){
+
+            idades[i] = i*i;
+        }
+
+        for (int i = 0; i < idades.length; i++){
+
+            System.out.println(idades[i]);
+        }
+    }
+}
