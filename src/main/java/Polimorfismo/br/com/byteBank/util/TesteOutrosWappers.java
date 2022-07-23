1e819c965feda7f5ffe9ac8a3c0e345aad9dd683
GabrielCarrijoF <carrijogit2003@gmail.com>
2022-07-23 00:25:26 -0300

    refactor: class number

---
+++ src/main/java/Polimorfismo/br/com/byteBank/util/TesteOutrosWappers.java
@@ -0,0 +1,25 @@
+package Polimorfismo.br.com.byteBank.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteOutrosWappers {
+
+    public static void main(String[] args) {
+
+        Integer ref = Integer.valueOf(29);
+        System.out.println(ref.doubleValue());
+
+        Double doubleRef =2.4;
+        System.out.println(doubleRef.doubleValue());
+
+        Boolean booleanRef = Boolean.TRUE;
+        System.out.println(booleanRef.booleanValue());
+
+        Number numero = Float.valueOf(29f);
+
+        List<Number> lista = new ArrayList<>();
+        lista.add(10);
+        lista.add(56.8);
+    }
+}
