b9f6bdbe261f140990a63c2ed0fd4caa5b1152f9
GabrielCarrijoF <carrijogit2003@gmail.com>
2022-08-01 17:04:34 -0300

    refactor: Scanner

--- src/main/java/java_io/src/Teste.java
+++ src/main/java/java_io/src/Teste.java
@@ -7,12 +7,25 @@ public class Teste {
 
     public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(new File("contas.csv"));
+        Scanner sc = new Scanner(new File("C:\\Users\\Gabriel\\Desktop\\Alura-Projetos-Java\\src\\main\\java\\java_io\\src\\contas.csv"));
 
         while (sc.hasNextLine()){
 
             String linha = sc.nextLine();
             System.out.println(linha);
+
+            Scanner scanner = new Scanner(linha);
+            scanner.useDelimiter(",");
+
+            String valor1 = scanner.next();
+            int valor2 = scanner.nextInt();
+            double valor3 = scanner.nextDouble();
+
+            System.out.println(valor1 + valor2 + valor3);
+
+
+//          String[] valores = linha.split(",");
+//          System.out.println(valores);
         }
         sc.close();
     }
